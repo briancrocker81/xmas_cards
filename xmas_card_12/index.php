8ad91c6af8d9ad8c4dfc9c29c65325e1b8bad936
Brian Crocker <briancrocker@southdevon.ac.uk>
2012-11-26 13:17:14 +0000

    added Nivo slider and soundcloud playback

---
+++ xmas_card_12/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html lang="en">
+<head>
+    <title>South Devon College 2012 Christmas Card</title>
+    <link rel="stylesheet" href="themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    <link rel="icon" type="image/png" href="http://southdevon.ac.uk/templates/sdcdefault/favicon.ico" />    
+</head>
+<body>
+    <div id="wrapper">
+        <div class="slider-wrapper theme-default">
+            <div id="slider" class="nivoSlider" style="width: 1024px; height: 768px;">
+                <img src="http://southdevon.ac.uk/images/news/all-news/1.jpg" data-thumb="images/toystory.jpg" alt="" />
+                <img src="http://southdevon.ac.uk/images/news/all-news/2.jpg" data-thumb="images/toystory.jpg" alt="" />
+                <img src="http://southdevon.ac.uk/images/news/all-news/3.jpg" data-thumb="images/toystory.jpg" alt="" />
+                <!--
+                <a href="http://dev7studios.com"><img src="images/up.jpg" data-thumb="images/up.jpg" alt="" title="This is an example of a caption" /></a>
+                <img src="images/walle.jpg" data-thumb="images/walle.jpg" alt="" data-transition="slideInLeft" />
+                <img src="images/nemo.jpg" data-thumb="images/nemo.jpg" alt="" title="#htmlcaption" /-->
+            </div>
+            <div id="htmlcaption" class="nivo-html-caption">
+                <strong>This</strong> is an example of a <em>HTML</em> caption with <a href="#">a link</a>. 
+            </div>
+<iframe style="float: right" width="40%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F22362952&amp;auto_play=true&amp;show_artwork=true&amp;color=4f2170"></iframe>  
+        </div>
+
+    </div>
+    <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="jquery.nivo.slider.js"></script>
+    <script type="text/javascript">
+    $(window).load(function() {
+        $('#slider').nivoSlider({
+          effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
+          animSpeed: 500, // Slide transition speed
+          pauseTime: 3000, // How long each slide will show
+          });
+    });
+    </script>  
+</body>
+</html>
